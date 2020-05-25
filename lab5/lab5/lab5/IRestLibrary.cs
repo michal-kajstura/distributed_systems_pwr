0bc2b0e2ec5f0c546e9b32fc0d2937e9eeed35ce
michalkaj <michalkajst@gmail.com>
2020-05-25 18:11:04 +0200

    lab 5

---
+++ lab5/lab5/lab5/IRestLibrary.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace lab5
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IRestLibrary
+    {
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/books",
+                ResponseFormat = WebMessageFormat.Json)]
+        List<Book> GetAll();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/books/{id}",
+                ResponseFormat = WebMessageFormat.Json)]
+        Book GetById(string id);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/books",
+              Method = "POST",
+              ResponseFormat = WebMessageFormat.Json)]
+        string Add(Book book);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "books/{id}",
+                   Method = "DELETE",
+                   ResponseFormat = WebMessageFormat.Json)]
+        string Delete(string id);
+
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "books/{id}",
+           Method = "PATCH",
+           BodyStyle =WebMessageBodyStyle.Wrapped,
+           ResponseFormat = WebMessageFormat.Json)]
+        string Update(string id, string title, string author, string year);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/info",
+                ResponseFormat = WebMessageFormat.Json)]
+        string Info();
+    }
+
+
+    [DataContract]
+    public class Book
+    {
+        [DataMember]
+        public int id;
+        [DataMember]
+        public string title;
+        [DataMember]
+        public string author;
+        [DataMember]
+        public int year;
+
+        public Book(int id, string title, string author, int year)
+        {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.year = year;
+        }
+
+    }
+}
---
+++ lab5/lab5/lab5/RestLibrary.svc.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace lab5
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class RestLibrary : IRestLibrary
+    {
+        private static List<Book> books;
+        const int INDEX1 = 242491;
+        const int INDEX2 = 242508;
+
+        RestLibrary()
+        {
+            books = new List<Book>()
+            {
+                new Book(1, "Czysty kod", "Rober C. Martin", 2014),
+                new Book(2, "Czystszy kod", "Rober RR. Martin", 2000),
+                new Book(3, "Test", "Testest", 2014),
+            };
+        }
+        public string Add(Book book)
+        {
+            if (book == null)
+                throw new WebFaultException<string>("400: Bad request", System.Net.HttpStatusCode.BadRequest);
+
+            var bookIdx = books.FindIndex(b => b.id == book.id);
+            if (bookIdx != -1)
+                throw new WebFaultException<string>("409: Book exists", System.Net.HttpStatusCode.Conflict);
+
+            if (book.author.Length < 3 
+                || book.author.All(c => char.IsLetter(c) || char.IsWhiteSpace(c))
+                || !char.IsUpper(book.author.ToCharArray()[0]) )
+                throw new WebFaultException<string>("400: Author name does not match the format", System.Net.HttpStatusCode.BadRequest);
+
+
+            books.Add(book);
+            return "Added item with id:" + book.id;
+        }
+
+        public string Delete(string id)
+        {
+            var intId = int.Parse(id);
+            var book = books.Find((b) => { return b.id == intId; });
+            if (book == null)
+                throw new WebFaultException<string>("404: Book not found", System.Net.HttpStatusCode.NotFound);
+
+            books.Remove(book);
+            return "Removed item with id:" + book.id;
+        }
+
+        public List<Book> GetAll()
+        {
+            return books;
+        }
+
+        public Book GetById(string id)
+        {
+            var intId = int.Parse(id);
+            var book = books.Find(b => b.id == intId);
+            if (book == null)
+                throw new WebFaultException<string>("404: Book not found", System.Net.HttpStatusCode.NotFound);
+            return book;
+        }
+
+        public string Info()
+        {
+            var date = DateTime.Now.ToString("dd/MM/yyyy h:mm tt");
+            return $"Imiona: Michał, Jakub\nIndeksy: {INDEX1}, {INDEX2}\nData i czas: {date}\n";
+        ;
+        }
+
+        public string Update(string id, string title, string author, string year)
+        {
+            var intId = int.Parse(id);
+            var book = books.Find(b => b.id == intId);
+            if (book == null)
+                throw new WebFaultException<string>("404: Book not found", System.Net.HttpStatusCode.NotFound);
+
+            book.title = title;
+            book.author = author;
+            book.year = int.Parse(year);
+
+            if (book.author.Length < 3
+                || book.author.Any(c => !char.IsLetter(c) && !char.IsWhiteSpace(c))
+                || !char.IsUpper(book.author.ToCharArray()[0]))
+                throw new WebFaultException<string>("400: Author name does not match the format", System.Net.HttpStatusCode.BadRequest);
+
+            return "Modified item with id:" + book.id;
+        }
+    }
+}
